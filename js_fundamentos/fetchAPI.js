00762d0dee2c0af2406e6bf65a7dd148e147a632
CiceroFernandes <cicerofernandessilva@gmail.com>
2023-05-31 23:53:45 -0300

    fetch api

--- js_fundamentos/fetchAPI.js
+++ js_fundamentos/fetchAPI.js
@@ -1,1 +1,12 @@
 //fetch
+
+fetch("https://pokeapi.co/api/v2/pokemon")
+  .then((res) => res.json())
+  .then((data) => {
+    // console.log(data);
+    console.log(data.results);
+    data.results.forEach((element) => {
+      //   console.log(element);
+      console.log(element.name);
+    });
+  });
