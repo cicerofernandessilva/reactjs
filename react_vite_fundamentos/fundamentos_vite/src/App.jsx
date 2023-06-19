e5aef806b439af21ebbac8380f0e1dac60cd997c
CiceroFernandes <cicerofernandessilva@gmail.com>
2023-06-19 01:38:55 -0300

    operador &&, reutilizar componentes, recorrer elementos con map, props, proptypes

--- react_vite_fundamentos/fundamentos_vite/src/App.jsx
+++ react_vite_fundamentos/fundamentos_vite/src/App.jsx
@@ -1,6 +1,10 @@
 // import { Fragment } from "react";
 // import React from "react";
 
+//import proptypes
+
+import PropTypes from "prop-types";
+
 //components
 const MyButton = () => {
   return <button>Hola React! Soy un boton!</button>;
@@ -25,6 +29,38 @@ const OfflineApp = () => {
   );
 };
 
+//operador &&
+const OperadorE = () => {
+  return (
+    <div>
+      <h4>Operador &&</h4>
+    </div>
+  );
+};
+
+//importando imagenes
+
+import img1 from "./assets/images/img_1.jpg";
+
+//uso del component props del boton
+const ButtonDin = (props) => {
+  return <button>{props.text}</button>;
+};
+
+//uso del proptypes
+
+ButtonDin.propTypes = {
+  text: PropTypes.string.isRequired,
+};
+
+// const ItemFrut = (props) => {
+//   return <li>{props.fruit}</li>;
+// };
+//props con desestructuraciob
+const ItemFrut = ({ fruit }) => {
+  return <li>{fruit}</li>;
+};
+
 const App = () => {
   //teste uso de fragment react
   //teste de interpolação
@@ -32,13 +68,13 @@ const App = () => {
   const classCenter = "text-center";
   const soma = 1 + 2;
   //ternário
-  const user = false;
+  const user = true;
+  const fruit = ["🍎", "🍏", "🍇", "🍐", "🍌", "🍉"];
   return (
     <>
       <div className="text-center">
         <h1 className="text-center">Hola React!</h1>
         <img src="https://picsum.photos/800/600" alt="" />
-
         {/* uso de interpolacion */}
         <h2 className={classCenter}>{titleH2}</h2>
         <p className={classCenter}>A soma de 1 mais 2 é {soma}</p>
@@ -49,6 +85,46 @@ const App = () => {
         ) : (
           <OfflineApp className={classCenter}></OfflineApp>
         )}
+        {user && <OperadorE></OperadorE>}
+        <img src={img1} alt="" />
+        <p>Imagene fixa</p>
+        {/* listas array*/}
+        <p>Array normal</p>
+        <ul>
+          <li>{fruit[0]}</li>
+          <li>{fruit[1]}</li>
+          <li>{fruit[2]}</li>
+          <li>{fruit[3]}</li>
+          <li>{fruit[4]}</li>
+          <li>{fruit[5]}</li>
+        </ul>
+        {/* listas array com map*/}
+        <p>Array con map</p>
+        <ul>
+          {fruit.map((fruit, index) => (
+            <li>
+              {fruit} = {index}
+            </li>
+          ))}
+        </ul>
+        <p>Array con map usando metodo key</p>
+        <ul>
+          {fruit.map((fruit, index) => (
+            <li key={index}>{fruit}</li>
+          ))}
+        </ul>
+        {/* uso del props */}
+        <ButtonDin text="buton1"></ButtonDin>
+        <ButtonDin text="buton2"></ButtonDin>
+        <ButtonDin text="buton3"></ButtonDin>
+        <ButtonDin text="buton4"></ButtonDin>
+
+        <p>Array con map usando metodo key y props</p>
+        <ul>
+          {fruit.map((fruit, index) => (
+            <ItemFrut key={index} fruit={fruit}></ItemFrut>
+          ))}
+        </ul>
       </div>
     </>
   );
