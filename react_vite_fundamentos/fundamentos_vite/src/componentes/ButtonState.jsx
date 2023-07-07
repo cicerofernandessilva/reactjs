d22ed2bfff6b28ce68a6f13d1293c1a6bf5751b3
CiceroFernandes <cicerofernandessilva@gmail.com>
2023-07-07 00:15:25 -0300

    hooks, useState, destructuracion de useState

--- react_vite_fundamentos/fundamentos_vite/src/componentes/ButtonState.jsx
+++ react_vite_fundamentos/fundamentos_vite/src/componentes/ButtonState.jsx
@@ -1,10 +1,27 @@
+import { useState } from "react";
+
 const ButtonState = () => {
-  let count = 0;
+  // let count = 0;
+  const arrayState = useState(0);
+  const stateNumber = arrayState[0];
+  const stateFunction = arrayState[1];
+  //destructuracion
+  const [count, setCount] = useState(0);
+
   const handleClick = () => {
-    count = count + 1;
-    console.log("HandleClick: " + count);
+    // count = count + 1;
+    // console.log("HandleClick: " + count);
+    stateFunction(stateNumber + 1);
   };
-  return <button onClick={handleClick}>Buttonstate: {count}</button>;
+  const handleClick2 = () => {
+    setCount(count + 1);
+  };
+  return (
+    <>
+      <button onClick={handleClick}>Buttonstate: {stateNumber}</button>
+      <button onClick={handleClick2}>Buttonstate: {count}</button>
+    </>
+  );
 };
 
 export default ButtonState;
