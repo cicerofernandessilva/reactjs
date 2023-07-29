c77f13cf9378de92cb84e7874124696b9a377e2d
CiceroFernandes <cicerofernandessilva@gmail.com>
2023-07-29 01:32:47 -0300

    useRef, caturando el form con formData

--- react_formularios/src/components/NoControlado.jsx
+++ react_formularios/src/components/NoControlado.jsx
@@ -1,9 +1,33 @@
+import { useRef } from "react";
+
 const NoControlado = () => {
   //   return "Formulário No Controlado";
+  const form = useRef(null);
+
   const handleSubmit = (e) => {
     e.preventDefault();
     //react já passa evento por default
     console.log("Me diste click!");
+    // console.log(form.current);
+
+    //capturar los datos
+
+    const data = new FormData(form.current);
+    console.log(data);
+    console.log(...data.entries());
+    const dataObject = Object.fromEntries([...data.entries()]);
+    console.log(dataObject, "dataObject");
+    const { title, descripcion, state } = Object.fromEntries([
+      ...data.entries(),
+    ]);
+
+    console.log(title, descripcion, state, "destructury");
+
+    //validar los datos
+    if (!title.trim()) return console.log("Llene este campo title!");
+    if (!descripcion.trim())
+      return console.log("Llene este campo descripcion!");
+    if (!state.trim()) return console.log("Llene este campo state!");
   };
 
   //prevenir submit por default
@@ -13,19 +37,25 @@ const NoControlado = () => {
 
   return (
     <div className="container mb-2">
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit} ref={form}>
         <input
           type="text"
           className="form-control mb-2"
           placeholder="Ingrese todo"
           name="title"
+          defaultValue="Cicero text #1"
         />
         <textarea
           name="descripcion"
           className="form-control mb-2"
           placeholder="Ingrese descripcion"
+          defaultValue="Cicero text #2"
         />
-        <select name="state" className="form-select mb-2">
+        <select
+          name="state"
+          className="form-select mb-2"
+          defaultValue="completado"
+        >
           <option value="pendiente">Pendiente</option>
           <option value="completado">Completado</option>
         </select>
